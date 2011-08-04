85eb9bf8502a9ca4a7fb03dc6f54f3113a044c90
Fabien Franzen <info@atelierfabien.be>
2011-08-04 15:03:57 +0200

    Re-removed reviews on products/show page - use local lib/site_hooks.rb instead

--- lib/spree_reviews_hooks.rb
+++ lib/spree_reviews_hooks.rb
@@ -1,9 +1,12 @@
-Deface::Override.new(
-  :virtual_path => "products/show",
-  :name => "reviews_after_product_properties",
-  :insert_after => "[data-hook='product_properties']",
-  :partial => "shared/reviews",
-  :disabled => false)
+# Place this in your local lib/site_hooks.rb file to include the partial
+# on the products/show page.
+#
+# Deface::Override.new(
+#   :virtual_path => "products/show",
+#   :name => "reviews_after_product_properties",
+#   :insert_after => "[data-hook='product_properties']",
+#   :partial => "shared/reviews",
+#   :disabled => false)
 
 Deface::Override.new(
   :virtual_path => "layouts/spree_application",
