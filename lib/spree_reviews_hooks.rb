73f417b1e950917a23b65fc462841dadcd179940
Fabien Franzen <info@atelierfabien.be>
2011-08-10 08:19:02 +0200

    Fixed hooks

--- lib/spree_reviews_hooks.rb
+++ lib/spree_reviews_hooks.rb
@@ -4,21 +4,21 @@
 # Deface::Override.new(
 #   :virtual_path => "products/show",
 #   :name => "reviews_after_product_properties",
-#   :insert_after => "[data-hook='product_properties']",
+#   :insert_after => "[data-hook='product_properties'], #product-properties[data-hook]",
 #   :partial => "shared/reviews",
 #   :disabled => false)
 
 Deface::Override.new(
   :virtual_path => "layouts/spree_application",
   :name => "reviews_inside_head",
-  :insert_bottom => "[data-hook='inside_head']",
+  :insert_bottom => "[data-hook='inside_head'], #inside-head[data-hook]",
   :text => "<%= stylesheet_link_tag('reviews.css') %>",
   :disabled => false)
 
 Deface::Override.new(
-  :virtual_path => "shared/_footer",
+  :virtual_path => "layouts/spree_application",
   :name => "reviews_footer_right",
-  :insert_after => "[data-hook='footer_right']",
+  :insert_after => "[data-hook='footer'], #footer[data-hook]",
   :text => "<%= javascript_include_tag('jquery.rating.js') %>
   <%= javascript_tag(\"$(document).ready(function(){$('.stars').rating({required:true});});\") %>",
   :disabled => false)
@@ -26,13 +26,13 @@ Deface::Override.new(
 Deface::Override.new(
   :virtual_path => "admin/shared/_product_sub_menu",
   :name => "reviews_admin_product_sub_tabs",
-  :insert_bottom => "[data-hook='admin_product_sub_tabs']",
+  :insert_bottom => "[data-hook='admin_product_sub_tabs'], #admin_product_sub_tabs[data-hook]",
   :text => "<%= tab(:reviews, :label => 'review_management') %>",
   :disabled => false)
 
 Deface::Override.new(
   :virtual_path => "admin/configurations/index",
   :name => "reviews_admin_configurations_menu",
-  :insert_bottom => "[data-hook='admin_configurations_menu']",
+  :insert_bottom => "[data-hook='admin_configurations_menu'], #admin_configurations_menu[data-hook]",
   :text => "<%= configurations_menu_item(t('spree_reviews.settings'), admin_review_settings_path, t('spree_reviews.manage_settings')) %>",
   :disabled => false)
